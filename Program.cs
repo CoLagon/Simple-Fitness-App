2566eef506f90c4d9d68df6c9ffb327c470b0c66
CoLagon <colelogan19@yahoo.com>
2016-12-03 15:47:02 -0800

    added and refactored the code

--- Program.cs
+++ Program.cs
@@ -7,29 +7,54 @@ namespace SimpleFitnessApp
         static void Main()
         {
        
-            int runningTotal = 0;
-            bool keepGoing = true;
+            double runningTotal = 0;
 
-            while (keepGoing)
+            // Repeat until the user quits
+            while (true)
             {
                 // Prompt the user for minutes exercised
                 Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
           
                 string entry = Console.ReadLine();
-                if (entry == "quit")
+                if (entry.ToLower() == "quit")
                 {
-                    keepGoing = false;
+                    break;
                 }
-                else
+                try
                 {
-                    int minutes = int.Parse(entry);
-                    runningTotal += minutes;
+                    double minutes = double.Parse(entry);
+
+                    if (minutes <= 0)
+                    {
+                        Console.WriteLine(minutes + " is not an acceptable value");
+                        continue;
+                    }
+                    else if (minutes <= 10)
+                    {
+                        Console.WriteLine("Better than nothing, am I right?");
+                    }
+                    else if (minutes <= 30)
+                    {
+                        Console.WriteLine("Way to go hot stuff!");
+                    }
+                    else if (minutes <= 60)
+                    {
+                        Console.WriteLine("You must be a Ninja Warrior in training!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ok, now you're just showing off!");
+                    }
                     // Add minutes exercised to total
+                    runningTotal += minutes;
                     // Display total minutes exercised to the screen
                     Console.WriteLine("You've exercised " + runningTotal + " minutes.");
-                    
                 }
-                // Repeat until the user quits
+                catch (FormatException)
+                {
+                    Console.WriteLine("That is not valid input");
+                    continue;
+                }  
             }
             Console.WriteLine("Goodbye");
         }         
